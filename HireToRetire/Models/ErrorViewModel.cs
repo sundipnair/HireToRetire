6f4b22cf3e9ba68deb45c93446a20db0b8d97ddc
Sundip Nair <sundip.nair@bjss.com>
2018-10-14 20:29:00 +0100

    Add project files.

---
+++ HireToRetire/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HireToRetire.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
