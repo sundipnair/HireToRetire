1ab871db142010270668d41c83e56592739ca0ca
Sundip Nair <sundip.nair@bjss.com>
2018-10-15 11:48:35 +0100

    first commit

---
+++ HireToRetire/Controllers/CandidateController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HireToRetire.Models;
+using HireToRetire.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HireToRetire.Controllers
+{
+    public class CandidateController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<CandidateViewModel> candidates = new ApiService(new Uri("http://40.118.66.69"))
+                .GetAsync<List<CandidateViewModel>>(new Uri("http://40.118.66.69/api/candidates")).Result;
+
+            return View(candidates);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult CreateSave(CandidateViewModel candidate)
+        {
+            // call api to save
+            new ApiService(new Uri("http://40.118.66.69")).PostAsync<CandidateViewModel>(new Uri("http://40.118.66.69/api/candidates"), candidate);
+            ViewData["Message"] = "Candidate successfully registered";
+            return View("Create");
+        }
+
+        public IActionResult Edit(CandidateViewModel candidate)
+        {
+            return View(candidate);
+        }
+
+        public IActionResult EditSave(CandidateViewModel candidate)
+        {
+            // call api to save
+            ViewData["Message"] = "Candidate successfully updated";
+            return View("Edit");
+        }
+
+        public IActionResult Details(CandidateViewModel candidate)
+        {
+            return View(candidate);
+        }
+
+        public IActionResult Delete(CandidateViewModel candidate)
+        {
+            return View(candidate);
+        }
+
+        public IActionResult DeleteSave(CandidateViewModel candidate)
+        {
+            // call api to delete
+            ViewData["Message"] = "Candidate successfully deleted";
+            return View("Delete");
+        }
+    }
+}
---
+++ HireToRetire/Service/ApiService.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireToRetire.Service
+{
+    public partial class ApiService
+    {
+
+        private readonly HttpClient _httpClient;
+        private Uri BaseEndpoint { get; set; }
+
+        public ApiService(Uri baseEndpoint)
+        {
+            if (baseEndpoint == null)
+            {
+                throw new ArgumentNullException("baseEndpoint");
+            }
+            BaseEndpoint = baseEndpoint;
+            _httpClient = new HttpClient();
+        }
+
+        /// <summary>  
+        /// Common method for making GET calls  
+        /// </summary>  
+        internal async Task<T> GetAsync<T>(Uri requestUrl)
+        {
+            addHeaders();
+            var response = await _httpClient.GetAsync(requestUrl, HttpCompletionOption.ResponseHeadersRead);
+            response.EnsureSuccessStatusCode();
+            var data = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+
+        /// <summary>  
+        /// Common method for making POST calls  
+        /// </summary>  
+        internal async void PostAsync<T>(Uri requestUrl, T content)
+        {
+            addHeaders();
+            var response = await _httpClient.PostAsync(requestUrl.ToString(), CreateHttpContent<T>(content));
+            response.EnsureSuccessStatusCode();
+            //var data = await response.Content.ReadAsStringAsync();
+            //return JsonConvert.DeserializeObject<Message<T>>(data);
+        }
+
+        private Uri CreateRequestUri(string relativePath, string queryString = "")
+        {
+            var endpoint = new Uri(BaseEndpoint, relativePath);
+            var uriBuilder = new UriBuilder(endpoint);
+            uriBuilder.Query = queryString;
+            return uriBuilder.Uri;
+        }
+
+        private HttpContent CreateHttpContent<T>(T content)
+        {
+            var json = JsonConvert.SerializeObject(content, MicrosoftDateFormatSettings);
+            return new StringContent(json, Encoding.UTF8, "application/json");
+        }
+
+        private static JsonSerializerSettings MicrosoftDateFormatSettings
+        {
+            get
+            {
+                return new JsonSerializerSettings
+                {
+                    DateFormatHandling = DateFormatHandling.MicrosoftDateFormat
+                };
+            }
+        }
+
+        private void addHeaders()
+        {
+            //_httpClient.DefaultRequestHeaders.Remove("userIP");
+            //_httpClient.DefaultRequestHeaders.Add("userIP", "192.168.1.1");
+        }
+    }
+}
