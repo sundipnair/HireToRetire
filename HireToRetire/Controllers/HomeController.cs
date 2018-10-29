0e1b89e461e682f8032c229d030d33f902a2d0fd
P95048488 <P95048488@capita.co.uk>
2018-10-29 09:19:27 +0000

    Egress Test Point

--- HireToRetire/Controllers/HomeController.cs
+++ HireToRetire/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using HireToRetire.Models;
 using Microsoft.AspNetCore.Authorization;
+using System.Net.Http;
 
 namespace HireToRetire.Controllers
 {
@@ -36,6 +37,20 @@ namespace HireToRetire.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public string TestEgress()
+        {
+            HttpClient client = new HttpClient();
+            string res = "TEstEgress default result";
+
+            HttpResponseMessage response = client.GetAsync("https://reqres.in/api/users/2").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                res = response.Content.ReadAsStringAsync().Result;
+            }
+
+            return res;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
